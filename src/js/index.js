50195676f18fd8e4b7e277f1228722d1a4265c08
DavideCapuozzo <devsoloweb@gmail.com>
2024-04-19 19:29:32 +0200

    loader
    insert loader pt2

--- src/js/index.js
+++ src/js/index.js
@@ -73,53 +73,64 @@ form.addEventListener('submit', function sendResource(event) {
         let fetchResponse = json;
         
         let works = fetchResponse.works
-        for (let i = 0; i < works.length; i++) {
-            let title = works[i].title;
-            let author = works[i].authors[0]['name'];
-            let descriptionId = works[i].key
-            let descriptionUrl = 'https://openlibrary.org'+descriptionId+'.json'
-            let availability = works[i].availability
-
-            const card = createElement('div', 'col-xxl-3', 'card', '', () => toggleDescription(descriptionUrl, card));
-            container.appendChild(card);
-            card.classList.add('col-lg-4');
-            card.classList.add('col-md-6');
-            
-
-            if(availability){
-                let isbn = availability['isbn'];
-                if(isbn){
-                    let linkImg = 'https://covers.openlibrary.org/b/isbn/'+isbn+'-M.jpg'
+        if(works == 0){
+            document.getElementById('loader').style.display = 'none';
+            let allert = createElement('h3', 'allert');
+            container.appendChild(allert);
+            allert.innerHTML = 'LA RICERCA NON HA RESTITUTITO NESSUN RISULTATO';
+        }else{
+            for (let i = 0; i < works.length; i++) {
+                let title = works[i].title;
+                let author = works[i].authors[0]['name'];
+                let descriptionId = works[i].key
+                let descriptionUrl = 'https://openlibrary.org'+descriptionId+'.json'
+                let availability = works[i].availability
     
-                    let imgRespnse = createPhotoElement(linkImg, '', 'imgCover')
-                    card.appendChild(imgRespnse);
-                } else{
-                    let linkImg = 'https://www.shutterstock.com/shutterstock/photos/2059817444/display_1500/stock-vector-no-image-available-photo-coming-soon-illustration-vector-2059817444.jpg'
+                const card = createElement('div', 'col-xxl-3', 'card', '', () => toggleDescription(descriptionUrl, card));
+                container.appendChild(card);
+                card.classList.add('col-lg-4');
+                card.classList.add('col-md-6');
+                
     
+                if(availability){
+                    let isbn = availability['isbn'];
+                    if(isbn){
+                        let linkImg = 'https://covers.openlibrary.org/b/isbn/'+isbn+'-M.jpg'
+        
+                        let imgRespnse = createPhotoElement(linkImg, '', 'imgCover')
+                        card.appendChild(imgRespnse);
+                    } else{
+                        let linkImg = 'https://www.shutterstock.com/shutterstock/photos/2059817444/display_1500/stock-vector-no-image-available-photo-coming-soon-illustration-vector-2059817444.jpg'
+        
+                        let imgRespnse = createPhotoElement(linkImg, '', 'imgCover')
+                        card.appendChild(imgRespnse);
+                    }
+                    
+                }else{
+                    let linkImg = 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'
                     let imgRespnse = createPhotoElement(linkImg, '', 'imgCover')
                     card.appendChild(imgRespnse);
                 }
+    
+                document.getElementById('loader').style.display = 'none';
+    
+                let titleRespnse = createElement('h5', 'title-book');
+                card.appendChild(titleRespnse)
+    
+                let authorsRespnse = createElement('p', 'authors-book');
+                card.appendChild(authorsRespnse);
+    
+                titleRespnse.innerHTML = title;
+                authorsRespnse.innerHTML = author;
                 
-            }else{
-                let linkImg = 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'
-                let imgRespnse = createPhotoElement(linkImg, '', 'imgCover')
-                card.appendChild(imgRespnse);
-            }
+                console.log('----- TITOLO ---- \n', works[i].title);
+                console.log('----- AUTORE ---- \n', works[i].authors[0]['name']);
+                console.log('----- DESCRIZIONE ---- \n', descriptionUrl);
+                
+            } 
+        }
 
-            let titleRespnse = createElement('h5', 'title-book');
-            card.appendChild(titleRespnse)
-
-            let authorsRespnse = createElement('p', 'authors-book');
-            card.appendChild(authorsRespnse);
-
-            titleRespnse.innerHTML = title;
-            authorsRespnse.innerHTML = author;
-            
-            console.log('----- TITOLO ---- \n', works[i].title);
-            console.log('----- AUTORE ---- \n', works[i].authors[0]['name']);
-            console.log('----- DESCRIZIONE ---- \n', descriptionUrl);
-            
-        } 
+        
     })
     .catch(error => console.error('Errore durante il fetch:', error));
 });
